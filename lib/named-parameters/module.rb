611756f3f34664e375363ba252af8f505faabded
Juris Galang <jgalang@host-104-1-0-10.sfoffice.verticalresponse.com>
2010-11-11 16:59:42 -0800

    Added support for has_named_parameter declaration for class methods.

--- lib/named-parameters/module.rb
+++ lib/named-parameters/module.rb
@@ -15,13 +15,24 @@
 #   FooBar.new :x => '...' # instantiates FooBar
 #   FooBar.new :y => '...' # ArgumentError - since :x was not specified
 #
+# @author Juris Galang
+#
+class Object  
+  def eigenclass # :nodoc:
+    class << self; self; end  
+  end  
+end
+
 module NamedParameters
-  def self.included base
+  def self.included base # :nodoc:
     base.extend ClassMethods
   end
 
   private
-  def self.validate name, params, spec
+  # this is the method used to validate the name of the received parameters 
+  # (based on the defined spec) when an instrumented method is invoked.
+  #
+  def self.validate name, params, spec  # :nodoc:
     spec[:required] ||= []
     spec[:optional] ||= []
     
@@ -54,36 +65,83 @@ module NamedParameters
   
   module ClassMethods
     protected
-    def method_added name
-      if specs.include?(name) && !instrumenting?
-        @instrumenting = true
-        method = instance_method(name)
-        spec   = specs.delete(name)
-        define_method name do |*args, &block|
-          params = args.find{ |arg| arg.instance_of? Hash }
-          name   = "#{self.class.name}##{name}"
-          NamedParameters::validate name, params || {}, spec
-          method.bind(self).call(*args, &block)
-        end
-        @instrumenting = false
-      end
-      super
-    end
-
+    # Declares that `method` will enforce named parameters behavior as 
+    # described in `spec`; a method declared with `required` and/or `optional`
+    # parameters will raise an `ArgumentError` if it is invoked without its
+    # required parameters or receives an unrecognized parameter.
+    #
+    # Sample usage:
+    #
+    #     has_named_parameters :point, :required => [ :x, :y ], 
+    #       :optional => :color
+    #
+    # @param [Symbol] `method` the name of the method that is supposed to 
+    #   enforce named parameters behavior.
+    #
+    # @param [Hash] `spec` the list of required and optional parameters. 
+    #
+    # @return [Hash] the specified `spec` 
+    #
     def has_named_parameters method, spec = { }
       specs[method] = spec
     end
     
+    # add instrumentation for class methods
+    def singleton_method_added name  # :nodoc:
+      instrument name do
+        method = self.eigenclass.instance_method name
+        spec   = specs.delete name
+        owner  = "#{self.name}."
+        eigenclass.instance_eval do
+          intercept method, owner, name, spec
+        end
+      end
+      super
+    end
+    
+    # add instrumentation for instance methods
+    def method_added name  # :nodoc:
+      instrument name do
+        method = instance_method name
+        spec   = specs.delete name
+        owner  = "#{self.name}#"
+        intercept method, owner, name, spec
+      end
+      super
+    end
+
     private
-    def specs
+    # apply instrumentation to method
+    def instrument method  # :nodoc:
+      if specs.include? method and !instrumenting?
+        @instrumenting = true
+        yield method
+        @instrumenting = false
+      end
+    end
+    
+    # insert parameter validation prior to executing the instrumented method
+    def intercept method, owner, name, spec  # :nodoc:
+      define_method name do |*args, &block|
+        params = args.find{ |arg| arg.instance_of? Hash }
+        name   = "#{owner}#{name}"
+        NamedParameters::validate name, params || {}, spec
+        method.bind(self).call(*args, &block)
+      end
+    end
+    
+    # initialize specs table as needed 
+    def specs  # :nodoc:
       @specs ||= { }
     end
     
-    def instrumenting?
+    # check if in the process of instrumenting a method
+    def instrumenting?  # :nodoc:
       @instrumenting
     end
     
-    def self.extended base
+    # initialize the @instrumenting instance variable (housekeeping)
+    def self.extended base  # :nodoc:
       base.instance_variable_set :@instrumenting , false
     end
   end
