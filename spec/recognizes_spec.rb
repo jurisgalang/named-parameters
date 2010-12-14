e6f916a638681fe97bafa3a02156417d3b765a66
Juris Galang <jgalang@host-184-6-0-10.sfoffice.verticalresponse.com>
2010-12-14 12:24:03 -0800

    more specs

--- spec/recognizes_spec.rb
+++ spec/recognizes_spec.rb
@@ -1,41 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe NamedParameters do
+describe "NamedParameters::recognizes" do
   it "doesn't complain if you pass it an optional parameter" do
-    class Foo1
+    class Recognizes1
       recognizes :bar
       def initialize opts = {}; end
     end
-    lambda { Foo1.new :bar => :bar }.should_not raise_error(ArgumentError)
+    lambda { Recognizes1.new :bar => :bar }.should_not raise_error(ArgumentError)
   end
 
   it "doesn't complain if you don't pass it an optional parameter" do
-    class Foo2
+    class Recognizes2
       recognizes :bar
       def initialize opts = {}; end
     end
-    lambda { Foo2.new }.should_not raise_error(ArgumentError)
+    lambda { Recognizes2.new }.should_not raise_error(ArgumentError)
   end
 
   it "complains if you pass it an undeclared parameter" do
-    class Foo3
+    class Recognizes3
       recognizes :bar
       def initialize opts = {}; end
     end
-    lambda { Foo3.new :foo => :foo }.should raise_error(ArgumentError)
-    lambda { Foo3.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
+    lambda { Recognizes3.new :foo => :foo }.should raise_error(ArgumentError)
+    lambda { Recognizes3.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
   end
 
   it "allows more than one optional parameters" do
-    class Foo4
+    class Recognizes4
       recognizes :foo, :bar, :baz
       def initialize opts = {}; end
     end
-    lambda { Foo4.new :foo => :foo }.should_not raise_error(ArgumentError)
-    lambda { Foo4.new :bar => :bar }.should_not raise_error(ArgumentError)
-    lambda { Foo4.new :baz => :baz }.should_not raise_error(ArgumentError)
-    lambda { Foo4.new :foo => :foo, :bar => :bar }.should_not raise_error(ArgumentError)
-    lambda { Foo4.new :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
-    lambda { Foo4.new :foo => :foo, :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :foo => :foo }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :bar => :bar }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :baz => :baz }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :foo => :foo, :bar => :bar }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
+    lambda { Recognizes4.new :foo => :foo, :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
+  end
+
+  it "allows you to specify default values for optional parameters" do
+    class Recognizes5
+      recognizes :foo => :foo
+      def initialize opts = {}
+        @foo = opts[:foo]
+      end
+    end
+    recognizes = Recognizes5.new
+    recognizes.instance_variable_get(:@foo).should eql :foo
   end
 end
--- spec/requires_spec.rb
+++ spec/requires_spec.rb
@@ -1,46 +1,60 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe NamedParameters do
+describe "NamedParameters::requires" do
   it "complains if you don't pass it a required parameter" do
-    class Foo1
+    class Requires1
       requires :bar
       def initialize opts = {}; end
     end
-    lambda { Foo1.new }.should raise_error(ArgumentError)
+    lambda { Requires1.new }.should raise_error(ArgumentError)
   end
 
   it "doesn't complain if you pass it a required parameter" do
-    class Foo2
+    class Requires2
       requires :bar
       def initialize opts = {}; end
     end
-    lambda { Foo2.new :bar => :bar }.should_not raise_error(ArgumentError)
+    lambda { Requires2.new :bar => :bar }.should_not raise_error(ArgumentError)
   end
 
   it "complains if you pass it an undeclared parameter" do
-    class Foo3
+    class Requires3
       requires :bar
       def initialize opts = {}; end
     end
-    lambda { Foo3.new :foo => :foo }.should raise_error(ArgumentError)
-    lambda { Foo3.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
+    lambda { Requires3.new :foo => :foo }.should raise_error(ArgumentError)
+    lambda { Requires3.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
   end
   
   it "allows more than one required parameters" do
-    class Foo4
+    class Requires4
       requires :foo, :bar, :baz
       def initialize opts = {}; end
     end
-    lambda { Foo4.new :foo => :foo, :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
+    lambda { Requires4.new :foo => :foo, :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
   end
 
   it "complains if not all of the required parameters was passed" do
-    class Foo5
+    class Requires5
       requires :foo, :bar, :baz
       def initialize opts = {}; end
     end
-    lambda { Foo5.new :foo => :foo }.should raise_error(ArgumentError)
-    lambda { Foo5.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
-    lambda { Foo5.new :bar => :bar, :baz => :baz }.should raise_error(ArgumentError)
+    lambda { Requires5.new :foo => :foo }.should raise_error(ArgumentError)
+    lambda { Requires5.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
+    lambda { Requires5.new :bar => :bar, :baz => :baz }.should raise_error(ArgumentError)
   end
+
+  # TODO: This feature is still not yet implemented; expect it to fail
+  #it "allows you to declare multiple requires clause and treat it as one" do
+  #  class Requires6
+  #    requires :foo
+  #    requires :bar
+  #    requires :baz
+  #    def initialize opts = {}; end
+  #  end
+  #  lambda { Requires6.new :foo => :foo, :bar => :bar, :baz => :baz }.should_not raise_error(ArgumentError)
+  #  lambda { Requires6.new :foo => :foo }.should raise_error(ArgumentError)
+  #  lambda { Requires6.new :foo => :foo, :bar => :bar }.should raise_error(ArgumentError)
+  #  lambda { Requires6.new :bar => :bar, :baz => :baz }.should raise_error(ArgumentError)
+  #end
 end
